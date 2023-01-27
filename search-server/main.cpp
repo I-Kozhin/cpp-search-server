ea8720519e87b5c84c60831d31845e28d4a0f628
I-Kozhin <99353622+I-Kozhin@users.noreply.github.com>
2023-01-27 15:52:30 +0300

    Added document relevance, relevance-based search

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -6,72 +6,75 @@
 #include <string>
 #include <utility>
 #include <vector>
- 
+
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
- 
+
+// Объявите перечислимый тип DocumentStatus со значениями ACTUAL, IRRELEVANT, BANNED, REMOVED
+enum class DocumentStatus{
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED
+};
+
 string ReadLine() {
     string s;
     getline(cin, s);
     return s;
 }
 
-int ReadLineWithNumber() { // This function are needed to divide a number of strings and strings
-    int result = 0;
+int ReadLineWithNumber() {
+    int result;
     cin >> result;
     ReadLine();
     return result;
 }
- 
-vector<string> SplitIntoWords(const string& text) { // From string, consisted of words, to vector of words
+
+vector<string> SplitIntoWords(const string& text) {
     vector<string> words;
     string word;
     for (const char c : text) {
         if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
+            words.push_back(word);
+            word = "";
         } else {
             word += c;
         }
     }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
- 
+    words.push_back(word);
+    
     return words;
 }
- 
+    
 struct Document {
     int id;
     double relevance;
-    int rating; // a new point
+    int rating;
 };
 
-class SearchServer { // This is the main
+class SearchServer {
 public:
     void SetStopWords(const string& text) {
         for (const string& word : SplitIntoWords(text)) {
             stop_words_.insert(word);
         }
-    }
- 
-    void AddDocument(int document_id, const string& document, const vector<int>& ratings) { //const vector<int>& ratings новое
-        ++document_count_;
+    }    
+
+    void AddDocument(int document_id, const string& document, DocumentStatus status, const vector<int>& ratings) { // added DocumentStatus status
         const vector<string> words = SplitIntoWordsNoStop(document);
         const double inv_word_count = 1.0 / words.size();
         for (const string& word : words) {
             word_to_document_freqs_[word][document_id] += inv_word_count;
         }
-        document_ratings_.emplace(document_id, ComputeAverageRating(ratings)); // добавление и рассчёт рейтинга через функцию
+        documents_.emplace(document_id, DocumentData{ComputeAverageRating(ratings), status}); // status is saved in new struct DocumentData
     }
- 
-    vector<Document> FindTopDocuments(const string& raw_query) const {
+
+    vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status = DocumentStatus::ACTUAL) const { // поиск только по актуальным DocumentStatus status = DocumentStatus::ACTUAL           
         const Query query = ParseQuery(raw_query);
-        auto matched_documents = FindAllDocuments(query);
- 
+        auto matched_documents = FindAllDocuments(query, status);
+        
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
                  return lhs.relevance > rhs.relevance;
@@ -81,28 +84,21 @@ public:
         }
         return matched_documents;
     }
- 
+    
 private:
-    struct QueryWord {
-        string data;
-        bool is_minus;
-        bool is_stop;
+    struct DocumentData {
+        int rating;
+        DocumentStatus status;
     };
- 
-    struct Query {
-        set<string> plus_words;
-        set<string> minus_words;
-    };
- 
-    int document_count_ = 0;
+
     set<string> stop_words_;
     map<string, map<int, double>> word_to_document_freqs_;
-    map<int, int> document_ratings_;
- 
+    map<int, DocumentData> documents_;
+    
     bool IsStopWord(const string& word) const {
         return stop_words_.count(word) > 0;
     }
- 
+    
     vector<string> SplitIntoWordsNoStop(const string& text) const {
         vector<string> words;
         for (const string& word : SplitIntoWords(text)) {
@@ -112,7 +108,21 @@ private:
         }
         return words;
     }
- 
+    
+    static int ComputeAverageRating(const vector<int>& ratings) {
+        int rating_sum = 0;
+        for (const int rating : ratings) {
+            rating_sum += rating;
+        }
+        return rating_sum / static_cast<int>(ratings.size());
+    }
+    
+    struct QueryWord {
+        string data;
+        bool is_minus;
+        bool is_stop;
+    };
+    
     QueryWord ParseQueryWord(string text) const {
         bool is_minus = false;
         // Word shouldn't be empty
@@ -120,9 +130,18 @@ private:
             is_minus = true;
             text = text.substr(1);
         }
-        return {text, is_minus, IsStopWord(text)};
+        return {
+            text,
+            is_minus,
+            IsStopWord(text)
+        };
     }
- 
+    
+    struct Query {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+    
     Query ParseQuery(const string& text) const {
         Query query;
         for (const string& word : SplitIntoWords(text)) {
@@ -137,26 +156,13 @@ private:
         }
         return query;
     }
- 
+    
+    // Existence required
     double ComputeWordInverseDocumentFreq(const string& word) const {
-        return log(document_count_ * 1.0 / word_to_document_freqs_.at(word).size());
+        return log(documents_.size() * 1.0 / word_to_document_freqs_.at(word).size());
     }
 
-    static int ComputeAverageRating(const vector<int>& ratings) { // Теперь рассчёт рейтинга в статичном методе внутри класса, т.е. функцию ComputeAverageRating я просто загнал внутрь класса
-        if (ratings.empty()) {
-            return 0;
-        }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
-        // static_cast позволяет привести значение к типу int
-        // без использования дополнительной переменной
-        return rating_sum / static_cast<int>(ratings.size());
-
-    }
- 
-    vector<Document> FindAllDocuments(const Query& query) const {
+    vector<Document> FindAllDocuments(const Query& query, DocumentStatus status) const {
         map<int, double> document_to_relevance;
         for (const string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
@@ -164,10 +170,12 @@ private:
             }
             const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
             for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
-                document_to_relevance[document_id] += term_freq * inverse_document_freq;
+                if (documents_.at(document_id).status == status){
+                    document_to_relevance[document_id] += term_freq * inverse_document_freq;}
+                
             }
         }
- 
+        
         for (const string& word : query.minus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
@@ -176,50 +184,40 @@ private:
                 document_to_relevance.erase(document_id);
             }
         }
- 
+
         vector<Document> matched_documents;
         for (const auto [document_id, relevance] : document_to_relevance) {
-            matched_documents.push_back({document_id, relevance, document_ratings_.at(document_id)}); //добавлено document_ratings_.at(document_id)
+            matched_documents.push_back({
+                document_id,
+                relevance,
+                documents_.at(document_id).rating
+            });
         }
         return matched_documents;
     }
 };
- 
-SearchServer CreateSearchServer() {
-    SearchServer search_server;
-    search_server.SetStopWords(ReadLine());
- 
-    const int document_count = ReadLineWithNumber();
-    for (int document_id = 0; document_id < document_count; ++document_id) {
-        const string document = ReadLine();
-        int ratings_size;
-        cin >> ratings_size;
-        
-        // создали вектор размера ratings_size из нулей
-        vector<int> ratings(ratings_size, 0);
-        
-        // считали каждый элемент с помощью ссылки
-        for (int& rating : ratings) {
-            cin >> rating;
-        }
-        
-        search_server.AddDocument(document_id, document, ratings);
-        ReadLine();
-    }
-    
-    return search_server;
+
+void PrintDocument(const Document& document) {
+    cout << "{ "s
+         << "document_id = "s << document.id << ", "s
+         << "relevance = "s << document.relevance << ", "s
+         << "rating = "s << document.rating
+         << " }"s << endl;
 }
- 
 int main() {
-    const SearchServer search_server = CreateSearchServer();
-    
-    //Поменяли вывод, так как добавилось ещё одно поле
-    const string query = ReadLine();
-    for (const Document& document : search_server.FindTopDocuments(query)) {
-        cout << "{ "
-             << "document_id = " << document.id << ", "
-             << "relevance = " << document.relevance << ", "
-             << "rating = " << document.rating
-             << " }" << endl;
+    SearchServer search_server;
+    search_server.SetStopWords("и в на"s);
+    search_server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
+    search_server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
+    search_server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
+    cout << "ACTUAL:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::ACTUAL)) {
+        PrintDocument(document);
     }
-}
+    cout << "BANNED:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::BANNED)) {
+        PrintDocument(document);
+    }
+    return 0;
+} 
